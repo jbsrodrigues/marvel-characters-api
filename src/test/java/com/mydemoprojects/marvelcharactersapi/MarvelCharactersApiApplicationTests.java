dfb2c662f15607446ccd38b5bcbb43a619fbd11e
brodrigues <bernardino.rodrigues@evolok.com>
2019-10-27 21:20:50 +0000

    Unit tests

---
+++ src/test/java/com/mydemoprojects/marvelcharactersapi/MarvelCharactersApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mydemoprojects.marvelcharactersapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarvelCharactersApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ src/test/java/com/mydemoprojects/marvelcharactersapi/controllers/CharacterControllerTest.java
@@ -0,0 +1,38 @@
+package com.mydemoprojects.marvelcharactersapi.controllers;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest
+public class CharacterControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private CharacterController characterController;
+
+    @Test
+    public void getAllCharacters() throws Exception {
+        Integer[] characters = {1, 2, 3, 4, 5};
+        given(characterController.getAllCharacters()).willReturn(characters);
+
+        mockMvc.perform(get("/characters"))
+                .andExpect(status().isOk())
+                .andExpect(content().json("[1,2,3,4,5]"));
+    }
+
+    //TODO implement missing tests
+
+}
